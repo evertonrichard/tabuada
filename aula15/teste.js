ac7f8a6a1a0f4463f780deffa9afd81a704547de
everton <evertonrichard444@gmail.com>
2023-05-26 14:22:00 -0300

    funções básicas do javascript.

--- aula15/teste.js
+++ aula15/teste.js
@@ -1,6 +1,6 @@
-let valores = [1, 5, 4, 5, 6, 6, 9]
+let valores = [1, 2, 3, 4, 5, 6, 7, 8]
 valores.sort()
-for(let pos in valores){
-    console.log(`a posição ${pos} tem o valor de ${valores[pos]}`)
+for (let pos in valores){
+    console.log(`a posição ${pos} tem o elemento ${valores[pos]}`)
 }
 // versão simplificada do recurso de repetição
